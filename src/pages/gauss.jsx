9ed40156a48b37bab38db308128f74108b76c2ae
andika <zoneandika@gmail.com>
2023-04-18 08:27:01 +0700

    rubah component gauss dari class menjadi component

--- src/pages/gauss.jsx
+++ src/pages/gauss.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { Link } from "react-router-dom";
@@ -9,41 +9,36 @@ const mapStyle = {
   height: "500px",
 };
 
-class Gauss extends Component {
-  componentDidMount() {
-    // inisialisasi peta
-    this.map = L.map("mapid").setView([0, 0], 13);
-
-    // buat tile layer dengan OpenStreetMap
+function Gauss() {
+  useEffect(() => {
+    const map = L.map("mapid").setView([0, 0], 13);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© OpenStreetMap contributors",
-    }).addTo(this.map);
-
-    // tambahkan marker pada lokasi tertentu
-    var marker = L.marker([-6.1753924, 106.8271528]).addTo(this.map);
-
-    // tambahkan popup pada marker
+    }).addTo(map);
+    const marker = L.marker([-6.1753924, 106.8271528]).addTo(map);
     marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-  }
 
-  render() {
-    return (
-      <div>
-        <h1 className="font-inter text-[2rem] text-center py-[50px]">
-          peta output Gauss
-        </h1>
-        <div className="w-[90%] mx-auto rounded-[10px]">
-          <div id="mapid" className="w-[50%] mx-auto" style={mapStyle}></div>
-        </div>
-        <Link
-          to="/"
-          className="py-[50px] text-center mx-auto w-fit block text-blue-400"
-        >
-          kembali ke homepage
-        </Link>
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1 className="font-inter text-[2rem] text-center py-[50px]">
+        peta output Gauss
+      </h1>
+      <div className="w-[90%] mx-auto rounded-[10px]">
+        <div id="mapid" className="w-[50%] mx-auto" style={mapStyle}></div>
       </div>
-    );
-  }
+      <Link
+        to="/"
+        className="py-[50px] text-center mx-auto w-fit block text-blue-400"
+      >
+        kembali ke homepage
+      </Link>
+    </div>
+  );
 }
 
 export default Gauss;
